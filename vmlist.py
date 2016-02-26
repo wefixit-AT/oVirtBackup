4da482e067e5021fc4620f573d3cad883b9bc59b
Fedora Cloud User <scotthesterberg@users.noreply.github.com>
2016-02-26 15:11:49 -0500

    fix filename variable, removed uneeded comments

--- vmlist.py
+++ vmlist.py
@@ -3,55 +3,29 @@
 import sys
 import os
 import re
-#from ovirtsdk.api import API 
-#from ovirtsdk.xml import params
-#from threading import Thread
 import time
 import logging
 
 #Configure
  
-#APIURL="https://engine.example.com/api/"
-#APIUSER="admin@internal"
-#APIPASS="securepassword:)"
-#CAFILE="ca.crt"
-#FILENAME="config.cfg"
-LOGFILENAME="list_setup.log"
+LOGconfig_file="list_setup.log"
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
-                    filename=LOGFILENAME,
+                    filename=LOGconfig_file,
                     filemode='w')
-def get_vm_list (vms):
+def get_vm_list (vms,config_file):
 	try:
 		vmstring="vm_names: ["
 		for vm in vms[:-1]:
 			vmstring += '"'+vm.name+'"'+", " 
 		vmstring +='"'+vms[-1].name+'"'+"]"
 		#print vmstring 
-		with open(FILENAME) as myfile:
+		with open(config_file) as myfile:
 			out=open("config.tmp","w")
 			for line in myfile:
 				out.write(re.sub("^vm_names.*$", vmstring, line))
 			out.close()
-			os.rename("config.tmp", FILENAME)
+			os.rename("config.tmp", config_file)
 	except Exception as ex:
                logging.debug('Unexpected error: %s' % ex)
-#if __name__ == "__main__":
-#   	try:	
-#        	api = API(url=APIURL,
-#                      username=APIUSER,
-#              	      password=APIPASS,
-#                      ca_file=CAFILE)
-#    		try: 
-#			print ' \n I am logging in %s \n' % LOGFILENAME
-#
-#			getObjInfo(api.vms.list(max=400))
-#
-#    		except Exception as e:
-#        		logging.debug('Error:\n%s' % str(e))
-#		
-#    		api.disconnect()
-#	
-#	except Exception as ex:
-#   		logging.debug('Unexpected error: %s' % ex)
