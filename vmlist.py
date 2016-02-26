a06f62d6999d31d7a39c8d13252aba12b2f00b97
Fedora Cloud User <scotthesterberg@users.noreply.github.com>
2016-02-26 14:33:47 -0500

    further commented uneeded code

--- vmlist.py
+++ vmlist.py
@@ -3,19 +3,19 @@
 import sys
 import os
 import re
-from ovirtsdk.api import API 
-from ovirtsdk.xml import params
-from threading import Thread
+#from ovirtsdk.api import API 
+#from ovirtsdk.xml import params
+#from threading import Thread
 import time
 import logging
 
 #Configure
  
-APIURL="https://engine.example.com/api/"
-APIUSER="admin@internal"
-APIPASS="securepassword:)"
-CAFILE="ca.crt"
-FILENAME="config.cfg"
+#APIURL="https://engine.example.com/api/"
+#APIUSER="admin@internal"
+#APIPASS="securepassword:)"
+#CAFILE="ca.crt"
+#FILENAME="config.cfg"
 LOGFILENAME="list_setup.log"
 
 logging.basicConfig(level=logging.DEBUG,
@@ -23,17 +23,20 @@ logging.basicConfig(level=logging.DEBUG,
                     filename=LOGFILENAME,
                     filemode='w')
 def get_vm_list (vms):
-	vmstring="vm_names: ["
-	for vm in vms[:-1]:
-		vmstring += '"'+vm.name+'"'+", " 
-	vmstring +='"'+vms[-1].name+'"'+"]"
-	#print vmstring 
-	with open(FILENAME) as myfile:
-		out=open("config.tmp","w")
-		for line in myfile:
-			out.write(re.sub("^vm_names.*$", vmstring, line))
-		out.close()
-		os.rename("config.tmp", FILENAME)
+	try:
+		vmstring="vm_names: ["
+		for vm in vms[:-1]:
+			vmstring += '"'+vm.name+'"'+", " 
+		vmstring +='"'+vms[-1].name+'"'+"]"
+		#print vmstring 
+		with open(FILENAME) as myfile:
+			out=open("config.tmp","w")
+			for line in myfile:
+				out.write(re.sub("^vm_names.*$", vmstring, line))
+			out.close()
+			os.rename("config.tmp", FILENAME)
+	except Exception as ex:
+               logging.debug('Unexpected error: %s' % ex)
 #if __name__ == "__main__":
 #   	try:	
 #        	api = API(url=APIURL,
