78693bf971ffe131f1244b924f65ec4e02dc5632
Fedora Cloud User <scotthesterberg@users.noreply.github.com>
2016-02-26 14:06:14 -0500

    commented out main, changed names, moving towards use as submethod of backup.py

--- vmlist.py
+++ vmlist.py
@@ -18,17 +18,15 @@ CAFILE="ca.crt"
 FILENAME="config.cfg"
 LOGFILENAME="list_setup.log"
 
-s1= '.'
-
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     filename=LOGFILENAME,
                     filemode='w')
-def getObjInfo (objlist):
+def get_vm_list (vms):
 	vmstring="vm_names: ["
-	for obj in objlist[:-1]:
-		vmstring += '"'+obj.name+'"'+", " 
-	vmstring +='"'+objlist[-1].name+'"'+"]"
+	for vm in vms[:-1]:
+		vmstring += '"'+vm.name+'"'+", " 
+	vmstring +='"'+vms[-1].name+'"'+"]"
 	#print vmstring 
 	with open(FILENAME) as myfile:
 		out=open("config.tmp","w")
@@ -36,21 +34,21 @@ def getObjInfo (objlist):
 			out.write(re.sub("^vm_names.*$", vmstring, line))
 		out.close()
 		os.rename("config.tmp", FILENAME)
-if __name__ == "__main__":
-   	try:	
-        	api = API(url=APIURL,
-                      username=APIUSER,
-              	      password=APIPASS,
-                      ca_file=CAFILE)
-    		try: 
-			print ' \n I am logging in %s \n' % LOGFILENAME
-
-			getObjInfo(api.vms.list(max=400))
-
-    		except Exception as e:
-        		logging.debug('Error:\n%s' % str(e))
-		
-    		api.disconnect()
-	
-	except Exception as ex:
-   		logging.debug('Unexpected error: %s' % ex)
+#if __name__ == "__main__":
+#   	try:	
+#        	api = API(url=APIURL,
+#                      username=APIUSER,
+#              	      password=APIPASS,
+#                      ca_file=CAFILE)
+#    		try: 
+#			print ' \n I am logging in %s \n' % LOGFILENAME
+#
+#			getObjInfo(api.vms.list(max=400))
+#
+#    		except Exception as e:
+#        		logging.debug('Error:\n%s' % str(e))
+#		
+#    		api.disconnect()
+#	
+#	except Exception as ex:
+#   		logging.debug('Unexpected error: %s' % ex)
