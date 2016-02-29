25aab1c82f58b21eeb8147f6ff2a94328b5ca47b
Fedora Cloud User <scotthesterberg@users.noreply.github.com>
2016-02-29 14:35:33 -0500

    fixed logging in line with rest of project, moved tmp file creation to /tmp

--- vmlist.py
+++ vmlist.py
@@ -4,16 +4,17 @@ import sys
 import os
 import re
 import time
-import logging
+#import logging
+from logger import Logger
 
 #Configure
  
-LOGconfig_file="list_setup.log"
+#LOGconfig_file="list_setup.log"
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    filename=LOGconfig_file,
-                    filemode='w')
+#logging.basicConfig(level=logging.DEBUG,
+#                    format='%(asctime)s %(levelname)s %(message)s',
+#                    filename=LOGconfig_file,
+#                    filemode='w')
 def get_vm_list (vms,config_file):
 	try:
 		vmstring="vm_names: ["
@@ -22,10 +23,11 @@ def get_vm_list (vms,config_file):
 		vmstring +='"'+vms[-1].name+'"'+"]"
 		#print vmstring 
 		with open(config_file) as myfile:
-			out=open("config.tmp","w")
+			out=open("/tmp/config.tmp","w")
 			for line in myfile:
 				out.write(re.sub("^vm_names.*$", vmstring, line))
 			out.close()
-			os.rename("config.tmp", config_file)
+			os.rename("/tmp/config.tmp", config_file)
 	except Exception as ex:
-               logging.debug('Unexpected error: %s' % ex)
+#               logging.debug('Unexpected error getting list of vms: %s' % ex)
+		Logger.log('Unexpected error getting list of vms: %s' % ex)
